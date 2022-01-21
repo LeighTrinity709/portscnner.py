2591f76b5f7e01eec7c5a3abeb8772e4101ef08c
Edwin Welling <ewelling@42solutions.nl>
2022-01-21 22:32:46 +0100

    pythonize

--- portscanner.py
+++ portscanner.py
@@ -1,47 +1,45 @@
-#LeighTrinity
-#Jan 18 2022
+# LeighTrinity
+# Jan 18 2022
 
 import socket
-from IPy import IP
 
 
-def scan(target):
-    converted_ip = check_ip(target)
-    print('\n +' '.....scanning target ' + str(target))
-    for port in range(1, 100):
-        scan_port(converted_ip, port)
-
-
-def check_ip(ip):
+def check_ip(hostname: str):
+    """Return the IP address of a given string.
+    if the given cannot be converted to a valid IP address
+    it return an empty string"""
     try:
-        IP(ip)
-        return(ip)
-    except ValueError:
-        return socket.gethostbyname(ip)
+        return socket.gethostbyname(hostname)
+    except socket.gaierror:
+        return ""
 
 
-
-def get_banner(s):
-    return s.recv(1024)
-
-
-def scan_port(ipaddress, port):
+def scan_port(ipaddress, port) -> tuple[bool, bytes]:
+    """Checks whether it is possible to connect to given
+    ip address and port. If possible to retrieve it also returns
+    the banner returned from the scanned server."""
+    opened = False
+    banner = bytes()
     try:
-        sock = socket.socket()
-        sock.settimeout(0.5)
-        sock.connect((ipaddress, port))
-        try:
-            banner = get_banner(sock)
-            print('[+]Open Port' + str(port) + ' : ' + str(banner))
-        except:
-            print('[+] Open Port' + str(port))
-
-    except:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)
+            s.connect((ipaddress, port))
+            opened = True
+            banner = s.recv(1024)
+    except socket.timeout as e:
         pass
+    return opened, banner
 
-targets = input('[+] Enter target/s to scan: (Split multiple targets with ,):  ')
-if ',' in targets:
-    for ip_add in targets.split(','):
-        scan(ip_add.strip(''))
-else:
-    scan(targets)
+
+def scan(target: str):
+    print(f"\n +.....scanning target {target}")
+    for port in range(1, 100):
+        print(f"{port} ", end="")
+        result = scan_port(target, port)
+        if result[0]:
+            print(f"\n[+]Open Port {port}: banner: {result[1]}")
+
+
+targets = input(f"[+] Enter target/s to scan: (Split multiple targets with ,):  ")
+for ip in [host for host in [check_ip(try_host) for try_host in targets.split(',')] if host]:
+    scan(ip)
